40881bb818f612967950695639a8a434ba27d311
Morgan McPeak <mcpeakmorgan@gmail.com>
2018-04-10 11:08:14 -0400

    various changes

--- script.js
+++ script.js
@@ -10,4 +10,40 @@ $(document).ready(function(){
             .end()
             .appendTo('#slideshow');
         },  4000);
+
+
+        $("#submit").click(function(){
+            var company = $("#company").val();
+            var title = $("#title").val();
+            var fname = $("#fname").val();
+            var lname = $("#lname").val();
+            var address = $("#address").val();
+            var city = $("#city").val();
+            var state = $("#state").val();
+            var zip = $("#zip").val();
+            var phone = $("#phone").val();
+            var industry = $("#industry").val();
+            var comments = $("#comments").val();
+            if(company == "" || title == "" || fname == "" || fname == "" || lname == "" ||  address == "" || city == "" ||   state == "" ||  zip == "" ||  phone == "" || industry == "" ||   comments == "") {
+                alert("Please fill out all required fields.");
+            } else {
+                $.post("contact-form.php", {
+                    company1: company,
+                    title1: title,
+                    fname1: fname,
+                    lname1: lname,
+                    address1: address,
+                    city1: city,
+                    state1: state,
+                    zip1: zip,
+                    phone1: phone,
+                    industry1: industry,
+                    comments1: comments
+                }, function(data) {
+                    if (data == "Your information has been recieved, we will contact you soon.") {
+                        $(".form")[0].reset();
+                    }
+                 });
+             }
+        });
     });
