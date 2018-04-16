14d7b0528d3c3a3125b211860d898c9f5059662b
Josh Klimaszewski <joshklimaszewski@Joshs-MacBook-Pro.local>
2018-04-16 18:57:42 -0400

    fixed php

--- mailer.php
+++ mailer.php
@@ -4,7 +4,7 @@
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Get the form fields and remove whitespace.
         $name = strip_tags(trim($_POST["fname"]));
-		$name = str_replace(array("\r","\n"),array(" "," "),$name);
+    	$name = str_replace(array("\r","\n"),array(" "," "),$name);
         $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
         $message = trim($_POST["message"]);
         $company = trim($_POST["company"]);
@@ -19,26 +19,14 @@
         $industry = trim($_POST["industry"]);
         $comments = trim($_POST["comments"]);
         // Check that data was sent to the mailer.
-        if ( empty($name) 
-        OR empty($company) 
-        OR empty($title)
-        OR empty($fname)
-        OR empty($lname)
-        OR empty($address)
-        OR empty($city)
-        OR empty($state)
-        OR empty($zip)
-        OR empty($phone)
-        OR empty($industry)
-        OR empty($comments)
-        OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        if ( empty($name) OR empty($company) OR empty($title) OR empty($fname) OR empty($lname) OR empty($address) OR empty($city) OR empty($state) OR empty($zip) OR empty($phone) OR empty($industry) OR empty($comments) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
             // Set a 400 (bad request) response code and exit.
-            http_response_code(400);
+#            http_response_code(400);
             echo "Oops! There was a problem with your submission. Please complete the form and try again.";
             exit;
         }
         // Set the recipient email address.
-        // FIXME: Update this to your desired email address.
+        
         $recipient = "joshua.klimaszewski@gmail.com";
 
         // Set the email subject.
@@ -63,17 +51,17 @@
         // Send the email.
         if (mail($recipient, $subject, $email_content, $email_headers)) {
             // Set a 200 (okay) response code.
-            http_response_code(200);
+#            http_response_code(200);
             echo "Thank You! Your message has been sent.";
         } else {
             // Set a 500 (internal server error) response code.
-            http_response_code(500);
+#            http_response_code(500);
             echo "Oops! Something went wrong and we couldn't send your message.";
         }
 
     } else {
         // Not a POST request, set a 403 (forbidden) response code.
-        http_response_code(403);
+#        http_response_code(403);
         echo "There was a problem with your submission, please try again.";
     }
 
--- script.js
+++ script.js
@@ -13,13 +13,12 @@ $(document).ready(function(){
 
     var form = $('#ajax-contact');
     var formMessages = $('#form-messages');
-    var formData = $(form).serialize();
+    
 
     $(form).submit(function(event) {
         event.preventDefault();
-    });
-
-    $.ajax({
+        var formData = $(form).serialize();
+        $.ajax({
         type: 'POST',
         url: $(form).attr('action'),
         data: formData
@@ -48,42 +47,5 @@ $(document).ready(function(){
             $(formMessages).text('Oops! An error occured and your message could not be sent.');
         }
     });
-
-
-
-
-        // $("#submit").click(function(){
-        //     var company = $("#company").val();
-        //     var title = $("#title").val();
-        //     var fname = $("#fname").val();
-        //     var lname = $("#lname").val();
-        //     var address = $("#address").val();
-        //     var city = $("#city").val();
-        //     var state = $("#state").val();
-        //     var zip = $("#zip").val();
-        //     var phone = $("#phone").val();
-        //     var industry = $("#industry").val();
-        //     var comments = $("#comments").val();
-        //     if(company == "" || title == "" || fname == "" || fname == "" || lname == "" ||  address == "" || city == "" ||   state == "" ||  zip == "" ||  phone == "" || industry == "" ||   comments == "") {
-        //         alert("Please fill out all required fields.");
-        //     } else {
-        //         $.post("contact.php", {
-        //             company1: company,
-        //             title1: title,
-        //             fname1: fname,
-        //             lname1: lname,
-        //             address1: address,
-        //             city1: city,
-        //             state1: state,
-        //             zip1: zip,
-        //             phone1: phone,
-        //             industry1: industry,
-        //             comments1: comments
-        //         }, function(data) {
-        //             if (data == "Your information has been recieved, we will contact you soon.") {
-        //                 $(".form")[0].reset();
-        //             }
-        //          });
-        //      }
-        // });
+    });
 });
